bfb7fcc2737857bb990df2294428428fac32c5a0
Johnny Hauser <johnnyhauser@gmail.com>
2017-10-14 17:17:11 -0500

    closure compiler

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -1,13 +1,23 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import svelte from 'rollup-plugin-svelte'
+import closure from 'rollup-plugin-closure-compiler-js'
 
 const Plugins = () => [
   resolve({
     module: true, browser: true, jsnext: true, main: true, extensions: [ '.js', '.json' ]
   }),
   commonjs(),
-  svelte({ cascade: false })
+  svelte({ cascade: false }),
+  closure({
+    languageIn: 'ECMASCRIPT6',
+    languageOut: 'ECMASCRIPT5',
+    compilationLevel: 'ADVANCED',
+    warningLevel: 'VERBOSE',
+    externs: [
+      // { src: `const global = window` }
+    ]
+  })
 ]
 
 export default [
